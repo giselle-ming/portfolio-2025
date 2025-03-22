850995d1d08d1a5c3ff2907144a1a486150e8cb6
giselle <giselle.mingue@gmail.com>
2025-03-22 19:41:41 -0400

    refactor: remove unused AboutSection and ContactSection imports from projects pageIntegrate Resend API for email functionality and enhance portfolio

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,10 +1,6 @@
-import Image from "next/image";
-import HeroSection from "./components/HeroSection";
-import Navbar from "./components/Navbar";
-import AboutSection from "./components/AboutSection";
-import ProjectsSection from "./components/ProjectsSection";
-import Footer from "./components/Footer";
-import ContactSection from "./components/ContactSection";
+import Navbar from "../components/Navbar";
+import AboutSection from "../components/AboutSection";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
